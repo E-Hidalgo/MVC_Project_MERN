f84c83bc9d1bb1076f9ec34a9cc2deed75219029
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2022-01-10 13:23:13 +0000

    fix-> minor issues (DB)

--- src/DB/index.js
+++ src/DB/index.js
@@ -3,5 +3,5 @@ import { DB_URI } from "../config";
 
 mongoose
   .connect(DB_URI)
-  .then((db) => console.log("DB CONNECTED"))
+  .then(() => console.log(`DB connected to ${DB_URI}`))
   .catch((err) => console.log(err));
